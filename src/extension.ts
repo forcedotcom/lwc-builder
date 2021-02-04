c9befbe3d0c220164bf95057a9bb770520f3c71f
Alba Rivas <arivas@salesforce.com>
2021-02-04 14:03:52 +0100

    Fix more linting warnings

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,6 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "lwc-builder" is now active!');
@@ -20,15 +19,18 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('lwc-builder.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
+  const disposable = vscode.commands.registerCommand(
+    'lwc-builder.helloWorld',
+    () => {
+      // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from lwc-builder!');
-  });
+      // Display a message box to the user
+      vscode.window.showInformationMessage('Hello World from lwc-builder!');
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+// export function deactivate() {}
