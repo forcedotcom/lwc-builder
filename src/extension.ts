db458cd8aead0dfc11103487a08f87acca93f3b7
Alba Rivas <arivas@salesforce.com>
2021-02-05 10:56:32 +0100

    Run linting & formatting Add CI jobs

--- src/extension.ts
+++ src/extension.ts
@@ -9,22 +9,28 @@ import * as vscode from 'vscode';
 import { getWebviewContent } from './lwcBuilder';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "lwc-builder" is now active!');
+  console.log('Congratulations, your extension "lwc-builder" is now active!');
 
-	let disposable = vscode.commands.registerCommand('lwc-builder.openLWCBuilder', () => {
-		vscode.window.showInformationMessage('Hello World from lwc-builder!');
-		const webviewPanel = vscode.window.createWebviewPanel(
-			'lwcBuilder',
-			'LWC Builder',
-			vscode.ViewColumn.One,
-			{
-				// Enable scripts in the webview
-				enableScripts: true
-			}
-		);
+  const disposable = vscode.commands.registerCommand(
+    'lwc-builder.openLWCBuilder',
+    () => {
+      vscode.window.showInformationMessage('Hello World from lwc-builder!');
+      const webviewPanel = vscode.window.createWebviewPanel(
+        'lwcBuilder',
+        'LWC Builder',
+        vscode.ViewColumn.One,
+        {
+          // Enable scripts in the webview
+          enableScripts: true,
+        }
+      );
 
-		webviewPanel.webview.html = getWebviewContent(context, webviewPanel.webview);
-	});
+      webviewPanel.webview.html = getWebviewContent(
+        context,
+        webviewPanel.webview
+      );
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
